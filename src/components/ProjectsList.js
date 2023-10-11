a2279e44e179fed44d44e55669bfa422e718a867
Aaron <aaronromeromusic@gmail.com>
2023-10-11 14:38:57 +0200

    blabla

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -9,4 +9,14 @@ export const projectsList = [
     image: './assets/previewfototiendaderopa.jpg',
     url: 'https://01-js-dom-proyecto-aaroncarrasco.netlify.app/',
   },
+  {
+    name: 'PROYECTO NASA',
+    image: './assets/nasaproject.jpg',
+    url: 'https://01-react-nasa.netlify.app/',
+  },
+  {
+    name: 'PROYECTO WEATHER APP',
+    image: './assets/weatherproject.jpg',
+    url: 'https://01-react-weather-project.netlify.app/',
+  },
 ];
